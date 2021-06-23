b97093ef3bf786098ad072e1e0e000981fe7f6f7
sujinkim123 <alicekim0429@naver.com>
2021-06-23 22:33:33 +0900

    기초5_2_문자열

---
+++ 기초5_2_문자열/src/_1131_문자출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class _1131_문자출력하기 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		char c = stdIn.next().charAt(0);
+		System.out.println(c);
+	}
+}
---
+++ 기초5_2_문자열/src/_1132_문자열출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class _1132_문자열출력하기 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		String word = stdIn.next();
+		System.out.println(word);
+	}
+}
