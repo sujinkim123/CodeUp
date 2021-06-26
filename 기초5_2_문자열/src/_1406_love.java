c39be153c55b04db5595e3e36e995bacc29aeece
sujinkim123 <alicekim0429@naver.com>
2021-06-26 22:55:14 +0900

    기초5_2_문자열

---
+++ 기초5_2_문자열/src/_1406_love.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _1406_love {
+	public static void main(String[] args) {
+		
+		Scanner stdIn = new Scanner(System.in);
+		String word = stdIn.next();
+		
+		if (word.equalsIgnoreCase("love")) {
+			System.out.println("I love you.");
+		}
+	}
+}
---
+++ 기초5_2_문자열/src/_1734_welcome.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _1734_welcome {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		String id = stdIn.next();
+		System.out.println("welcome! " + id);
+	}
+}
